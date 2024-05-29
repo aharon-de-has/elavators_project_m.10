eb542d1fb0a98b7874996897c496a61c758a7228
aharon de has <as077300@gmail.com>
2024-05-29 10:40:21 +0300

    wrote Bulding, Elevator, and Floor classes - first draft

---
+++ ElevetorProject.py
@@ -0,0 +1,43 @@
+import pygame
+import queue
+
+class Elevator():
+    def __init__(self):
+        self.__current_floor = 0
+        self.__still_available = 0
+        # self.__request = set()
+
+    
+class Floor():
+    def __init__(self, num):
+        self.__floor_num = num
+        mouse
+        timer
+
+        
+class Building:
+    def __init__(self, floors: int, elevators: int):
+        self.__floors = []
+        for i in range(floors):
+            self.__floors.append(Floor(i))
+        self.__elevators = []
+        for i in range(elevators):
+            self.__elevators.append(Elevator(i))
+        self.__request = queue.Queue()
+
+    def control(self, floor_num): 
+        self.__request.put(floor_num)
+        neareste = get_neareste_elevator()
+        if elvator == 0:
+            remove
+        # return eleator,timer
+
+
+    def get_neareest_elevator(self, ):
+
+    
+    
+
+
+
+
--- main.py
+++
@@ -1,9 +0,0 @@
-print("try git commit and push.")
-X = 12
-
-def helo():
-    print(helo)
-
-helo()
-
-print("helo world")
