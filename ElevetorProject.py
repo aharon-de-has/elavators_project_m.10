ae52eee9f4eb3fd52b5dc528fb5a028e2a68ce1f
aharon de has <as077300@gmail.com>
2024-05-29 13:10:27 +0300

    elevetor managment

--- ElevetorProject.py
+++ ElevetorProject.py
@@ -1,39 +1,73 @@
 import pygame
 import queue
+import numpy
 
-class Elevator():
-    def __init__(self):
-        self.__current_floor = 0
-        self.__still_available = 0
-        # self.__request = set()
+window_width = 700
+window_heigh = 500
+
+pygame.init()
+size = (window_width, window_heigh)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("game")
+
+pygame.quit()
+
+WHITE = (255, 255, 255)
+finish = False
+while not finish:
+    screen.fill(WHITE)
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+
+
+
+# class Elevator():
+#     def __init__(self):
+#         self.__current_floor = 0
+#         self.__till_available = 0
+#         self.__queue = queue.Queue()
+
+#     def move_elevator():
+#         pygame
 
     
-class Floor():
-    def __init__(self, num):
-        self.__floor_num = num
-        mouse
-        timer
+# class Floor():
+#     def __init__(self, num):
+#         self.__floor_num = num
+#         pygame.mouse
+#         pygame.timer
 
         
-class Building:
-    def __init__(self, floors: int, elevators: int):
-        self.__floors = []
-        for i in range(floors):
-            self.__floors.append(Floor(i))
-        self.__elevators = []
-        for i in range(elevators):
-            self.__elevators.append(Elevator(i))
-        self.__request = queue.Queue()
+# class Building:
+#     def __init__(self, floors: int, elevators: int):
+#         self.__floors = []
+#         for i in range(floors):
+#             self.__floors.append(Floor(i))
+#         self.__elevators = []
+#         for i in range(elevators):
+#             self.__elevators.append(Elevator(i))
+#         self.__request = queue.Queue()
 
-    def control(self, floor_num): 
-        self.__request.put(floor_num)
-        neareste = get_neareste_elevator()
-        if elvator == 0:
-            remove
-        # return eleator,timer
+    
 
+#     def get_neareste_elevator(self,__request):
+#         data_elevator = [] * len(self.__elevators)
+#         for elevator in self.__elevators:
+#             timer = self.__till_available + ((abs(self.current_floor - self.__floor_num)) * 0.5)
+#             if self.__till_available != 0:
+#                 timer += 2
+#             data_elevator[elevator] = timer
+#         self.__queue.put(data_elevator.index(min(data_elevator))) #need to be precise
+#         return min(data_elevator)
 
-    def get_neareest_elevator(self, ):
+    
+#     def control(self, floor_num): 
+#         self.__request.put(floor_num)
+#         neareste = get_neareste_elevator(__request) #need to be precise
+        
+
 
     
     
